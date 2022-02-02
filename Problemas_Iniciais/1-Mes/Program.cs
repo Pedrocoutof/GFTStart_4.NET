97576513efbc895ff35e36974b27376cc5766cd3
Pedro Couto <pedrocouto152001@gmail.com>
2022-02-02 09:58:03 -0300

    Desafios Iniciais

---
+++ Problemas_Iniciais/1-Mes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public enum Mes
+{
+    January = 1,
+    February = 2,
+    March = 3,
+    April = 4,
+    May = 5,
+    June = 6,
+    July = 7,
+    August = 8,
+    September = 9,
+    October = 10,
+    November = 11,
+    December = 12,
+
+}
+
+public class Problem
+{
+    public static void Main()
+    {
+
+        int valorMes = int.Parse(Console.ReadLine()!);
+        
+        if(valorMes >=1 && valorMes <=12){
+            Console.Write($"{(Mes)valorMes}");
+        }
+    
+        else{
+            Console.WriteLine("Digite um número válido de 1 a 12");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ Problemas_Iniciais/3-Soma de Pares Consecutivos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DIO
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+            int valorFinal;
+            int x = int.Parse(Console.ReadLine());
+            int contador = 0;
+
+            
+
+            while (x != 0)
+            {
+                valorFinal = 0;
+                contador = 0;
+
+                for (int i = 0; contador < 5; i++)
+                {
+                    if (((x + i) % 2) == 0)
+                    {
+                        contador++;
+                        valorFinal += x+i;
+                    }
+                }
+
+                Console.WriteLine($"{valorFinal}");
+                x = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
