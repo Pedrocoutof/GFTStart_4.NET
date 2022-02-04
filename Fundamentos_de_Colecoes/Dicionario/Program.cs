4ab80d674a0e6e8ceb12ed25f5333b0c34477f8f
Pedro Couto <pedrocouto152001@gmail.com>
2022-02-04 11:13:24 -0300

    Fundamentos de Colecoes

---
+++ Fundamentos_de_Colecoes/Dicionario/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//Dicionario de estados
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("MG", "Minas Gerais");
+estados.Add("RJ", "Rio de Janeiro");
+estados.Add("SP", "São Paulo");
+
+System.Console.WriteLine("Imprimindo todos estados");
+foreach(var item in estados){
+    System.Console.WriteLine($"{item.Key} - {item.Value}");
+}
+
+//Dicionario de meses
+Dictionary<int, string> mes = new Dictionary<int, string>();
+
+mes.Add(1, "Janeiro");
+mes.Add(2, "Fevereiro");
+mes.Add(3, "Março");
+mes.Add(4, "Abril");
+mes.Add(5, "Maio");
+mes.Add(6, "Junho");
+mes.Add(7, "Julho");
+mes.Add(8, "Agosto");
+mes.Add(9, "Setembro");
+mes.Add(10, "Outubro");
+mes.Add(11, "Novembro");
+mes.Add(12, "Dezembro");
+
+System.Console.WriteLine("Digite qual mes em numero que deseja pesquisar: ");
+int valor = int.Parse(System.Console.ReadLine()!);
+
+if(mes.ContainsKey(valor)){
+    System.Console.WriteLine($"{mes[valor]}");
+}
---
+++ Fundamentos_de_Colecoes/Operacoes_LINQ/Program.cs
@@ -0,0 +1,90 @@
+﻿class Program{
+
+    public static void imprimeArray(int [] array){
+        foreach(var elemento in array){
+            System.Console.Write($"{elemento}, ");
+        }
+    }
+    
+    public static void criaArrayPar(int[] arrayOriginal){
+
+        System.Console.WriteLine("\n\n: CRIA ARRAY PAR :");
+        System.Console.WriteLine("\nImprimindo array original");
+        imprimeArray(arrayOriginal);
+
+        //SINTAXE DE CONSULTA/QUERY
+        var arrayParC =  from elemento in arrayOriginal
+                        where elemento % 2 == 0
+                        orderby elemento
+                        select elemento;
+        
+        System.Console.WriteLine("\nImprimindo array par, utilizando a sintaxe de consulta/query");
+        imprimeArray(arrayParC.ToArray());
+
+    
+        //SINTAXE DE MÉTODO
+        var arrayParM = arrayOriginal.Where(elemento => elemento %2 == 0).OrderBy(elemento => elemento);
+        System.Console.WriteLine("\nImprimindo array par, utilizando a sintaxe de metodo");
+        imprimeArray(arrayParC.ToArray());
+    }
+
+    public static void criaArrayImpar(int[] arrayOriginal){
+        
+        System.Console.WriteLine("\n\n: CRIA ARRAY IMPAR :");
+        System.Console.WriteLine("\nImprimindo array original");
+        imprimeArray(arrayOriginal);
+
+        // UTILIZANDO SINTAXE DE CONSULTA/QUERY
+        var arrayImparC = from elemento in arrayOriginal
+                          where elemento % 2 != 0
+                          orderby elemento
+                          select elemento;
+        
+        System.Console.WriteLine("\nImprimindo array impar, utilizando a sintaxe de consulta/query");
+        imprimeArray(arrayImparC.ToArray());
+
+        // UTILIZANDO SINTAXE DE METODO
+
+        var arrayImparM = arrayOriginal.Where(elemento => elemento % 2 != 0).OrderBy(elemento => elemento);
+        System.Console.WriteLine("\nImprimindo array impar, utilizando a sintaxe de método");
+        imprimeArray(arrayImparM.ToArray());
+
+    }
+    
+    public static void criaArrayComValoresNegativos(int [] arrayOriginal){
+        
+        System.Console.WriteLine("\n\n: CRIA ARRAY COM VALORES NEGATIVOS :");
+        System.Console.WriteLine("\nImprimindo array original");
+        imprimeArray(arrayOriginal);
+
+        // UTILIZANDO CONSULTA
+        var arrayNegativoC = from elemento in arrayOriginal
+                             where elemento < 0
+                             orderby elemento
+                             select elemento;
+        
+        System.Console.WriteLine("\nImprimindo array de elementos negativos, utilizando a sintaxe de consulta/query");
+        imprimeArray(arrayNegativoC.ToArray());
+        
+
+        // UTILIZANDO METODO
+        var arrayNegativoM = arrayOriginal.Where(elemento => elemento < 0).OrderBy(elemento => elemento);
+        System.Console.WriteLine("\nImprimindo array de elementos negativos, utilizando a sintaxe de metodo");
+        imprimeArray(arrayNegativoC.ToArray());
+
+    }
+
+    public static void Main(){
+        
+        int [] array = new int[21];
+
+        for(int i = 0; i < 21; i++){
+            array[i] = i - 10;
+        }
+        
+        criaArrayPar(array);
+        criaArrayImpar(array);
+        criaArrayComValoresNegativos(array);
+
+    }
+}
